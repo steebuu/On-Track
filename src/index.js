b149ac1ee9621441b09638b80780dd9c52243c42
Steve <T.AllGameMail@protonmail.com>
2020-01-13 09:18:08 -0500

    changes name

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import "./styles/index.scss";
+import "./styles/map.scss";
 import "./styles/chart.scss";
+import "./styles/index.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   
-  const data = {
-    data1: "./data.csv",
-    data2: "./data2.csv"
-  }
-
-  let parse = file => {
-    d3.csv(file, d => d3.autoType(d)).then(data => {
-      window.DataExplorer = {};
-      DataExplorer.rawData = data;
-
-      
-    })
-  }
-  document.body.classList.add("center");
-  const card = document.createElement("div");
-  card.classList.add("card", "center");
-  card.innerHTML = `<h2>${greeting} World!</h2>`;
-  document.body.append(card);
 });
