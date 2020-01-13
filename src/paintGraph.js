044ba363fccf899583b5f810f6cccd996a94dc9b
Steve <T.AllGameMail@protonmail.com>
2020-01-12 20:30:25 -0500

    testing bar chart functionality with test data

--- src/paintGraph.js
+++ src/paintGraph.js
@@ -1,33 +1,41 @@
-const data = d3.csv("src/")
+const chart = d3.select('.chart');
 
-const paintGraph = (data) => {
-    console.log(data)
-    const chart = d3.select('.chart');
-    const width = +chart.attr('width')
-    const height = +chart.attr('height')
-    const xValue = d => d["Median Household Income"];
-    const yValue = d => d.name
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+const width = +chart.attr('width');
+const height = +chart.attr('height');
+
+const paintGraph = data => {
+    const xValue = d => d.population;
+    const yValue = d => d.country;
+    const margin = { top: 20, right: 20, bottom: 20, left: 100};
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
     const xScale = d3.scaleLinear()
         .domain([0, d3.max(data, xValue)])
-        .range([0, innerWidth])
-
+        .range([0, innerWidth]);
+    
     const yScale = d3.scaleBand()
-        .domain(Object.keys(data).map(yValue))
-        .padding(0.1);
-    
-    const f = svg.append('f')
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.15);
+
+    const g = chart.append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`)
-    
-    f.selectAll('rect').data(data)
+
+    g.append('g').call(d3.axisLeft(yScale))
+    g.append('g').call(d3.axisBottom(xScale))
+        .attr('transform', `translate(0,${innerHeight})`);
+
+    g.selectAll('rect').data(data)
         .enter().append('rect')
         .attr('y', d => yScale(yValue(d)))
-        .attr('width', d=> xScale(xValue(d)))
+        .attr('width', d => xScale(xValue(d)))
         .attr('height', yScale.bandwidth());
-    
-
-
 }
+
+d3.csv('src/test.csv').then(data => {
+    data.forEach(d => {
+        d.population = +d.population * 1000;
+    });
+    paintGraph(data);
+})
