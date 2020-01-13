f98f2cad09b5635c440025aa2e7fb5e127888485
Steve <T.AllGameMail@protonmail.com>
2020-01-12 23:58:46 -0500

    clears unnecessary comments, adds hover title to bar graph bars

--- src/map.js
+++ src/map.js
@@ -10,13 +10,10 @@ const pathGenerator = d3.geoPath().projection(projection);
 
 const g = svg.append('g');
 
-// svg.call(d3.zoom().on('zoom', () => {
-//     svg.attr('transform', d3.event.transform);
-// }))
-
 d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json").then(us => {
-    // console.log(us)
+
     const states = topojson.feature(us, us.objects.states);
+
     svg.selectAll('path').data(states.features)
         .enter().append('path')
         .attr('d', path)
@@ -29,19 +26,3 @@ d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json").then(u
         .attr('class', 'state-borders')
         .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; })));
 })
-
-// d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json", function(error, us) {
-//   if (error) throw error;
-//         console.log(us.objects)
-
-//         svg.append("g")
-//             .selectAll("path")
-//             .data(topojson.feature(us, us.objects.states).features)
-//             .enter().append("path")
-//             .attr("d", path)
-//             .attr("class", "states"); 
-
-//         svg.append("path")
-//             .attr("class", "state-borders")
-//             .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-//   });
